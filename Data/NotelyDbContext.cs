ff18015b422ab9505df0e1eccb3afc4dc32732a8
Md Nazim Uddin <isnamdnazim@gmail.com>
2022-10-13 22:08:23 +0600

    Added Note Model and DbContext class

---
+++ Data/NotelyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotelyRestApi.Models;
+
+namespace NotelyRestApi.Data
+{
+    public class NotelyDbContext : DbContext
+    {
+        public NotelyDbContext(DbContextOptions<NotelyDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Note> Notes => Set<Note>();
+    }
+}
---
+++ Models/Note.cs
@@ -0,0 +1,13 @@
+﻿namespace NotelyRestApi.Models
+{
+    public class Note
+    {
+        public long Id { get; set; }
+        public string Subject { get; set; } = string.Empty;
+        public string Details { get; set; } =string.Empty;
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModified { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
